1ebff90cd27c989e5fae1ec3f6ce0cd12b9069c9
duongnguyen96 <nguyenduonghd96@gmail.com>
2018-03-20 20:55:14 +0700

    Them ten trong index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def index
-	render html:"Hello world!"
+	render html:"Hello world! My name is Duong"
   end
 end
